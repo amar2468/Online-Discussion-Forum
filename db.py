f5403427178f087f4d2c4032919670ddbe3f5155
AmarPlakalo <114601596+AmarPlakalo@users.noreply.github.com>
2022-11-11 18:46:04 +0000

    Added forum database that deals with the forum topic

--- db.py
+++ db.py
@@ -4,5 +4,9 @@ from server import app
 
 CONNECTION_STRING = "mongodb+srv://CurrentUser:AluxwYqUCVDmkhba@cluster0.gghbacw.mongodb.net/?retryWrites=true&w=majority"
 client = pymongo.MongoClient(CONNECTION_STRING)
-db = client.get_database('RegisterLoginDatabase')
-user_collection = pymongo.collection.Collection(db, 'user_collection')
+
+register_login_database = client.get_database('RegisterLoginDatabase')
+forum_database = client.get_database('ForumDatabase')
+
+user_collection = pymongo.collection.Collection(register_login_database, 'user_collection')
+user_collection_2 = pymongo.collection.Collection(forum_database, 'user_collection')
