0798733830fb5d3ab001d92deb8ddb27ed9867fa
amar2468 <plakaloam@gmail.com>
2022-11-12 14:49:03 +0000

    Added more functionality to the "like post feature"

--- server.py
+++ server.py
@@ -146,15 +146,34 @@ def delete_topic(id_delete):
 
 	return redirect("/")
 
-# Route for like functionality
+# Route for like post functionality
 
 @app.route('/like_post/<like_post_id>')
 def like_post(like_post_id):
 
-	db.forum_database.ForumPostCollection.update_one(
-		{ '_id':  ObjectId(like_post_id) },
-		{ "$inc": { 'number_of_likes':  1} }
-	)
+	for document in db.forum_database.ForumPostCollection.find():
+		if str(document["_id"]) == like_post_id:
+
+			if session.get("name") == document["author_of_post"]:
+
+				if document["user_liked_own_post"] == False:
+
+					db.forum_database.ForumPostCollection.update_one(
+						{ '_id':  ObjectId(like_post_id) },
+						{ "$set": { 'user_liked_own_post':  True} }
+					)
+
+					db.forum_database.ForumPostCollection.update_one(
+						{ '_id':  ObjectId(like_post_id) },
+						{ "$inc": { 'number_of_likes':  1} }
+					)
+			
+			else:
+				db.forum_database.ForumPostCollection.update_one(
+					{ '_id':  ObjectId(like_post_id) },
+					{ "$inc": { 'number_of_likes':  1} }
+				)
+
 
 	return redirect("/")
 
@@ -166,12 +185,10 @@ def forum_post():
 		title = request.form.get("title_of_post")
 		content = request.form.get("post_content")
 
-		db.forum_database.ForumPostCollection.insert_one({"author_of_post":session.get("name"), "title_of_post": title, "content_of_post": content, "number_of_likes": 0 })
-		collection_info = db.forum_database.ForumPostCollection.find()
-		return render_template("home.html", collection_info=collection_info)
+		db.forum_database.ForumPostCollection.insert_one({"author_of_post":session.get("name"), "title_of_post": title, "content_of_post": content, "number_of_likes": 0, "user_liked_own_post": False })
+		return redirect('/')
 	elif not session.get("name"):
-		collection_info = db.forum_database.ForumPostCollection.find()
-		return render_template("home.html", collection_info=collection_info)
+		return redirect('/')
 
 if __name__ == '__main__':
 	app.run(debug=True)
