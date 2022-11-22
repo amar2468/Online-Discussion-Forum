79b96cd573db94809e8a9543ead95ed94850053a
amar2468 <plakaloam@gmail.com>
2022-11-22 19:46:29 +0000

    Added validation for reset password

--- static/main.js
+++ static/main.js
@@ -1,4 +1,4 @@
-function validate_form()
+function validate_register_form()
 {
     var password_user = document.getElementById("user_password_for_register").value;
     var confirm_password_user = document.getElementById("confirm_password_for_register").value;
@@ -6,10 +6,30 @@ function validate_form()
     if(password_user.length < 10)
     {
         alert("Password must be at least 10 characters. Enter a new password. ");
+        return false;
     }
 
     if(password_user != confirm_password_user)
     {
         alert("The password and the confirmation of the password do not match!");
+        return false;
+    }
+}
+
+function validate_reset_password_form()
+{
+    var password_reset = document.getElementById("password_reset").value;
+    var confirm_password_reset = document.getElementById("confirm_password_reset").value;
+
+    if(password_reset.length < 10)
+    {
+        alert("Password must be at least 10 characters. Enter a new password. ");
+        return false;
+    }
+
+    if(password_reset != confirm_password_reset)
+    {
+        alert("The password and the confirmation of the password do not match!");
+        return false;
     }
 }
