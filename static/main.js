34a1849e92c7a084da00c503147b655e0d473355
amar2468 <plakaloam@gmail.com>
2022-12-04 18:12:25 +0000

    Functionality that checks whether password contains numbers, special chars, uppercase + lowercase letters

--- static/main.js
+++ static/main.js
@@ -3,6 +3,8 @@ function validate_register_form()
     var password_user = document.getElementById("user_password_for_register").value;
     var confirm_password_user = document.getElementById("confirm_password_for_register").value;
     var check_age_for_register = document.getElementById("check_age_for_register");
+    let check_password_requirement = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{10,})')
+
 
     if (check_age_for_register.checked == false)
     {
@@ -18,6 +20,16 @@ function validate_register_form()
     if(password_user != confirm_password_user)
     {
         alert("The password and the confirmation of the password do not match!");
+        return false;
+    }
+
+    if(check_password_requirement.test(password_user))
+    {
+        return;
+    }
+    else
+    {
+        alert("Password must contain at least one uppercase character, one lowercase character, one number and a special character. ");
         return false;
     }
 }
@@ -39,3 +51,11 @@ function validate_reset_password_form()
         return false;
     }
 }
+
+/*
+function get_file_path_profile_picture()
+{
+    let file_path = document.getElementById("change_profile_picture_file_upload").value;
+
+}
+*/
