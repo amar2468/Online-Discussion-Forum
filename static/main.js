c1dec31ef63f9ec87b024ed5f7ef677a690cd0e3
amar2468 <plakaloam@gmail.com>
2022-12-08 17:26:41 +0000

    Added error checking for the register form + the reset password form. Displays error message if the value is not as required

--- static/main.js
+++ static/main.js
@@ -1,61 +1,145 @@
 function validate_register_form()
 {
-    var password_user = document.getElementById("user_password_for_register").value;
-    var confirm_password_user = document.getElementById("confirm_password_for_register").value;
-    var check_age_for_register = document.getElementById("check_age_for_register");
+    // Declaring variables that will be used to validate the register form
+
+    let first_name = document.getElementById("first_name").value;
+    let last_name = document.getElementById("last_name").value;
+    let email = document.getElementById("email_address_for_register").value;
+    let password_user = document.getElementById("user_password_for_register").value;
+    let confirm_password_user = document.getElementById("confirm_password_for_register").value;
+    let check_age_for_register = document.getElementById("check_age_for_register");
     let check_password_requirement = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{10,})')
 
+    // Referencing all the error paragraphs
+    let all_errors = document.querySelectorAll(".error");
 
-    if (check_age_for_register.checked == false)
+    // Iterate through each error
+	for(let error of all_errors)
     {
-        alert("You must be at least 16, in order to register an account: ");
+        // Hide the error message
+		error.style.display = "none";
+	}
+
+    // If the user does not enter the first name
+    if (first_name == "")
+    {
+        // Show the relevant error message as a block
+        document.querySelector(".name-error").innerHTML = "Please enter a name";
+        document.querySelector(".name-error").style.display = "block";
         return false;
     }
+
+    // If the user does not enter the last name
+    if (last_name == "")
+    {
+        // Show the relevant error message as a block
+        document.querySelector(".lastname-error").innerHTML = "Please enter a surname";
+        document.querySelector(".lastname-error").style.display = "block";
+        return false;
+    }
+
+    // If the user does not enter the email
+    if (email == "")
+    {
+        // Show the relevant error message as a block
+        document.querySelector(".email-error").innerHTML = "Please enter an email";
+        document.querySelector(".email-error").style.display = "block";
+        return false;
+    }
+
+    // If the password entered is less than 10 characters
     if(password_user.length < 10)
     {
-        alert("Password must be at least 10 characters. Enter a new password. ");
+        // Show the relevant error message as a block
+        document.querySelector(".password-error").innerHTML = "Password must be at least 10 characters. Enter a new password";
+        document.querySelector(".password-error").style.display = "block";
         return false;
     }
 
+    // If the password is not as required
+    if(!check_password_requirement.test(password_user))
+    {
+        if (password_user != "")
+        {
+            // Error message telling the user what needs to be contained within a password
+            document.querySelector(".password-error").innerHTML = "Password must contain at least one uppercase character, one lowercase character, one number and a special character";
+            document.querySelector(".password-error").style.display = "block";
+            return false;
+        }
+    }
+
+    // If the user does not confirm the password
+    if (confirm_password_user == "")
+    {
+        // Show the relevant error message as a block
+        document.querySelector(".confirmpass-error").innerHTML = "Please confirm your password";
+        document.querySelector(".confirmpass-error").style.display = "block";
+        return false;
+    }
+
+    // If the password entered does not match the field where the user confirms their password
     if(password_user != confirm_password_user)
     {
-        alert("The password and the confirmation of the password do not match!");
+        // Show the relevant error message as a block
+        document.querySelector(".confirmpass-error").innerHTML = "The password and the confirmation of the password do not match";
+        document.querySelector(".confirmpass-error").style.display = "block";
         return false;
     }
 
-    if(check_password_requirement.test(password_user))
+    // If the checkbox is not checked
+    if (check_age_for_register.checked == false)
     {
-        return;
-    }
-    else
-    {
-        alert("Password must contain at least one uppercase character, one lowercase character, one number and a special character. ");
+        // Show the relevant error message as a block
+        document.querySelector(".checkbox-error").innerHTML = "Please confirm whether you are over 16 years of age";
+        document.querySelector(".checkbox-error").style.display = "block";
         return false;
     }
 }
 
 function validate_reset_password_form()
 {
-    var password_reset = document.getElementById("password_reset").value;
-    var confirm_password_reset = document.getElementById("confirm_password_reset").value;
+    // Declaring the password variables that have been referenced from the html page
+    let password_reset = document.getElementById("password_reset").value;
+    let confirm_password_reset = document.getElementById("confirm_password_reset").value;
+    let check_password_requirement = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{10,})')
 
+    // Referencing all the error paragraphs
+    let all_errors = document.querySelectorAll(".error");
+
+    // Iterate through each error
+    for(let error of all_errors)
+    {
+        // Hide the error message
+        error.style.display = "none";
+    }
+
+    // If the password length is less than 10
     if(password_reset.length < 10)
     {
-        alert("Password must be at least 10 characters. Enter a new password. ");
+        // Error message telling the user to enter at least 10 characters for the password
+        document.querySelector(".password-error").innerHTML = "Password must be at least 10 characters. Enter a new password";
+        document.querySelector(".password-error").style.display = "block";
         return false;
     }
 
+    // If the password is not as required
+    if(!check_password_requirement.test(password_reset))
+    {
+        if (password_reset != "")
+        {
+            // Error message telling the user what needs to be contained within a password
+            document.querySelector(".password-error").innerHTML = "Password must contain at least one uppercase character, one lowercase character, one number and a special character";
+            document.querySelector(".password-error").style.display = "block";
+            return false;
+        }
+    }
+
+    // If the password is not equal to the one that was entered in the confirm password field
     if(password_reset != confirm_password_reset)
     {
-        alert("The password and the confirmation of the password do not match!");
+        // Error message telling the user that the passwords do not match
+        document.querySelector(".confirmpass-error").innerHTML = "The password and the confirmation of the password do not match";
+        document.querySelector(".confirmpass-error").style.display = "block";
         return false;
     }
 }
-
-/*
-function get_file_path_profile_picture()
-{
-    let file_path = document.getElementById("change_profile_picture_file_upload").value;
-
-}
-*/
