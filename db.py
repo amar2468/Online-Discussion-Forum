a4b1fb6fc2ca92fdf97c1ca8c307c6fe25b28968
amar2468 <plakaloam@gmail.com>
2023-02-14 11:36:55 +0000

    Created notifications database

--- db.py
+++ db.py
@@ -8,6 +8,8 @@ client = pymongo.MongoClient(CONNECTION_STRING)
 register_login_database = client.get_database('RegisterLoginDatabase')
 forum_database = client.get_database('ForumDatabase')
 subforum_database = client.get_database('Subforum')
+notifications_database = client.get_database('Notifications')
 
 user_collection = pymongo.collection.Collection(register_login_database, 'user_collection')
 user_collection_2 = pymongo.collection.Collection(forum_database, 'user_collection')
+user_collection_3 = pymongo.collection.Collection(notifications_database, 'user_collection')
