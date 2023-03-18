45125f9fa09033e51d7157abf86672bc0ccc36d4
amar2468 <plakaloam@gmail.com>
2023-03-18 11:49:01 +0000

    Removed all databases and put all the collections from those databases into the one database.

--- db.py
+++ db.py
@@ -5,11 +5,6 @@ import app
 CONNECTION_STRING = "mongodb+srv://CurrentUser:AluxwYqUCVDmkhba@cluster0.gghbacw.mongodb.net/?retryWrites=true&w=majority"
 client = pymongo.MongoClient(CONNECTION_STRING)
 
-register_login_database = client.get_database('RegisterLoginDatabase')
 forum_database = client.get_database('ForumDatabase')
-subforum_database = client.get_database('Subforum')
-notifications_database = client.get_database('Notifications')
 
-user_collection = pymongo.collection.Collection(register_login_database, 'user_collection')
 user_collection_2 = pymongo.collection.Collection(forum_database, 'user_collection')
-user_collection_3 = pymongo.collection.Collection(notifications_database, 'user_collection')
